5c13127d268fc4b8d99898c1c7ff6ce955fe1b37
cvardakis <158996240+cvardakis@users.noreply.github.com>
2025-07-31 22:37:56 -0600

    CLI INTEGRATION

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import speedtest
+import sys
+import json
+import subprocess
 import time
 from datetime import datetime, timedelta
 import os
@@ -15,22 +18,44 @@ def internet_test():
     return ping, download, upload
 
 
-def log_network_speed():
+def cli_internet_test():
+    try:
+        output = subprocess.check_output(["speedtest", "--json"], text=True)
+        data = json.loads(output)
+        ping = data["ping"]
+        download = round(data["download"] / 1_000_000, 2)  # Mbps
+        upload = round(data["upload"] / 1_000_000, 2)      # Mbps
+        return ping, download, upload
+    except subprocess.CalledProcessError as e:
+        raise RuntimeError("Failed CLI SpeedTest")
+
+
+def log_network_speed(mode):
     with open(RESULTS_FILE, "a") as file:
-        now = datetime.now()
-        test_results = internet_test()
-        file.write(f'{now.strftime("%b %m")}, {now.strftime("%H:%M")}, {test_results[0]}, {test_results[1]}, {test_results[2]}\n')
-        print(f'Results: {now.strftime("%b %m")}, {now.strftime("%H:%M")}, {test_results[0]}, {test_results[1]}, {test_results[2]}')
-
-
+        try:
+            now = datetime.now()
+            if mode == 0:
+                print("Testing with python package")
+                test_results = internet_test()
+            elif mode == 1:
+                print("Testing with CLI")
+                test_results = cli_internet_test()
+            else:
+                raise RuntimeError(f"Invalid Mode Provided: {mode}")
+        
+            file.write(f'{now.strftime("%b %m")}, {now.strftime("%H:%M")}, {test_results[0]}, {test_results[1]}, {test_results[2]}\n')
+            print(f'Results: {now.strftime("%b %m")}, {now.strftime("%H:%M")}, {test_results[0]}, {test_results[1]}, {test_results[2]}')
+        
+        except:
+            raise RuntimeError(f'Failed to collect data')
 
 def check_file():
     if not os.path.exists(RESULTS_FILE):
         with open(RESULTS_FILE, "a") as file:
-            file.write(f'Date, Time, Ping, Download, Upload')
+            file.write(f'Date, Time, Ping, Download, Upload\n')
 
 
-def monitor_network():
+def monitor_network(mode):
     interval = timedelta(minutes=TIME_INTERVAL)
     next_run = datetime.now()
     iteration = 0
@@ -39,7 +64,7 @@ def monitor_network():
         now = datetime.now()
         if now >= next_run:
             print(f'Testing Network: #{iteration}')
-            log_network_speed()
+            log_network_speed(mode)
             iteration += 1
             next_run += interval
         
@@ -50,8 +75,9 @@ def monitor_network():
 
 
 def main():
+    mode = int(sys.argv[1])
     check_file()
-    monitor_network()
+    monitor_network(mode)
 
 
 if __name__ == "__main__":
